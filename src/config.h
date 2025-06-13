c1fa57cf313720d37656b419d87939d44035806e
kaimac <kaimac@kaimac.org>
2025-06-13 05:47:48 +0100

    USB config

---
+++ src/config.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// USB identifiers
+#define CFG_USB_VID     0xCAFE
+#define CFG_USB_PID     0x4002
+#define CFG_USB_MFGR    "TinyUSB"
+#define CFG_USB_PRODUCT "TinyUSB Device"
+
+// Label of the disk as it appears on a computer
+#define CFG_VOLUME_LABEL "Synth"
--- src/hw/usb_descriptors.c
+++ src/hw/usb_descriptors.c
@@ -25,16 +25,7 @@
 
 #include "bsp/board_api.h"
 #include "tusb.h"
-
-/* A combination of interfaces must have a unique product id, since PC will save device driver after the first plug.
- * Same VID/PID with different interface e.g MSC (first), then CDC (later) will possibly cause system error on PC.
- *
- * Auto ProductID layout's Bitmap:
- *   [MSB]         HID | MSC | CDC          [LSB]
- */
-#define _PID_MAP(itf, n)  ( (CFG_TUD_##itf) << (n) )
-#define USB_PID           (0x4000 | _PID_MAP(CDC, 0) | _PID_MAP(MSC, 1) | _PID_MAP(HID, 2) | \
-                           _PID_MAP(MIDI, 3) | _PID_MAP(VENDOR, 4) )
+#include "config.h"
 
 //--------------------------------------------------------------------+
 // Device Descriptors
@@ -49,8 +40,8 @@ tusb_desc_device_t const desc_device =
     .bDeviceProtocol    = 0x00,
     .bMaxPacketSize0    = CFG_TUD_ENDPOINT0_SIZE,
 
-    .idVendor           = 0xCafe,
-    .idProduct          = USB_PID,
+    .idVendor           = CFG_USB_VID,
+    .idProduct          = CFG_USB_PID,
     .bcdDevice          = 0x0100,
 
     .iManufacturer      = 0x01,
@@ -154,8 +145,8 @@ enum {
 char const *string_desc_arr[] =
 {
   (const char[]) { 0x09, 0x04 }, // 0: is supported language is English (0x0409)
-  "TinyUSB",                     // 1: Manufacturer
-  "TinyUSB Device",              // 2: Product
+  CFG_USB_MFGR,                  // 1: Manufacturer
+  CFG_USB_PRODUCT,               // 2: Product
   NULL,                          // 3: Serials will use unique ID if possible
 };
 
